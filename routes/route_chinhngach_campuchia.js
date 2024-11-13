407c5362f4e7ae1d5427c4063e01642d073f9984
Tony Thịnh <nguyenphuthinhse@gmail.com>
2024-11-13 17:05:36 +0700

    Fixed line endings

--- routes/route_chinhngach_campuchia.js
+++ routes/route_chinhngach_campuchia.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getContactData } = require('../controllers/chinhngach_campuchia_controller');
+const { getContactData, UpdateData } = require('../controllers/chinhngach_campuchia_controller');
 
 // Định nghĩa route GET /contact
 router.get('/getCNCampuchia', getContactData);
+router.put('/updateData', UpdateData);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = require('./routes/index');
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 
 // Sử dụng routes cho API
 app.use('/api/v1', route);
